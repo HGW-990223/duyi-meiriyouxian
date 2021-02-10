2ec66d6bdbb4dee7fc04280c73a017253ff43fa4
黄广文 <18253094236@163.com>
2021-02-10 11:53:42 +0800

    系统原始布局

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <home></home>
+  </div>
+</template>
+
+<script>
+import Home from '@/views/Home'
+
+export default {
+  components: {
+    Home
+  }  
+};
+</script>
+
+
+<style lang="less" scoped>
+  #app{
+    height: 100%;
+  }
+</style>
